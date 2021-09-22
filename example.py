72744287f4fecf0086596b366ef11fbb4222009a
name <student-03-5929c390dd98@linux-instance.us-central1-a.c.qwiklabs-gcp-03-b805d23c8870.internal>
2021-09-22 10:49:52 +0000

    this my second commit

--- example.py
+++ example.py
@@ -1,3 +1,11 @@
 def git_operation():
 print("I am adding example.py file to the remote repository.")
 git_operation()
+print(validate-user("blue.kale",3))# true
+
+print(validate-user("blue.kale",3))# currently true,should be false
+
+print(validate-user("red_quinoa",4))# true
+
+print(validate-user("red_quinoa",4))# currently true,should be false
+
